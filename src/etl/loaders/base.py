36904d055430a46dc9245de1460bc94e2e8ddd69
XaverHeuser <xaver.heuser@gmail.com>
2025-10-15 20:44:23 +0200

    type error

--- src/etl/loaders/base.py
+++ src/etl/loaders/base.py
@@ -5,6 +5,6 @@ from abc import ABC, abstractmethod
 
 class BaseLoader(ABC):
     @abstractmethod
-    def load_data(self, data) -> None:
+    def load_data(self) -> None:
         """Takes transformed data and loads it into the data store."""
         pass
