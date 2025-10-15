3879a117270c0c126b8f1cab1078c8d5492a43af
XaverHeuser <xaver.heuser@gmail.com>
2025-10-15 21:48:49 +0200

    start

--- src/etl/extractors/base.py
+++ src/etl/extractors/base.py
@@ -5,6 +5,6 @@ from abc import ABC, abstractmethod
 
 class BaseExtractor(ABC):
     @abstractmethod
-    def fetch_activities(self) -> None:
+    def fetch_all_activities(self) -> None:
         """Fetches new activities and returns them in a standard format."""
         pass
