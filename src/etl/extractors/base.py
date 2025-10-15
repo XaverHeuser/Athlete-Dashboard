faaed9eb2b52bdecbd5d4643d6460a9dd392b506
XaverHeuser <95574334+XaverHeuser@users.noreply.github.com>
2025-10-15 22:14:17 +0200

    explore strava api (#4)
    * explore strava api
    
    * ruff and mypy

--- src/etl/extractors/base.py
+++ src/etl/extractors/base.py
@@ -1,10 +1,11 @@
 """This file implements ."""
 
 from abc import ABC, abstractmethod
+from typing import Any
 
 
 class BaseExtractor(ABC):
     @abstractmethod
-    def fetch_activities(self) -> None:
+    def fetch_all_activities(self) -> list[dict[str, Any]]:
         """Fetches new activities and returns them in a standard format."""
         pass
