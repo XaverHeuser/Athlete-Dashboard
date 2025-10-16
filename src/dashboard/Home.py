9642d77041035784d5bb22dc51e3aaa41dda44ce
XaverHeuser <xaver.heuser@gmail.com>
2025-10-16 18:08:08 +0200

    Start streamlit development

--- src/dashboard/Home.py
+++ src/dashboard/Home.py
@@ -1,1 +1,33 @@
 """Homepage of Dashboard."""
+
+import streamlit as st
+from queries import load_raw_data
+
+# --- Page Configuration ---
+st.set_page_config(
+    page_title="Athlete Dashboard",
+    page_icon="🏃",
+    layout="wide"
+)
+
+
+st.title('Athlete Dashboard')
+st.markdown("Visualizing personal activity data from Strava via BigQuery.")
+
+# Load the data from BigQuery
+try:
+    df_activities_raw = load_raw_data()
+
+    # --- Data Visualization ---
+    st.header("Monthly Activity Summary")
+    
+    if not df_activities_raw.empty:
+        # Display a bar chart of monthly distance
+        st.subheader("Raw Data")
+        st.write(len(df_activities_raw))
+        st.dataframe(df_activities_raw)
+    else:
+        st.warning("No activity data found in BigQuery.")
+
+except Exception as e:
+    st.error(f"An error occurred while loading data from BigQuery: {e}")
---
+++ src/dashboard/queries.py
@@ -0,0 +1,30 @@
+"""This module includes queries from bigquery for streamlit."""
+
+import streamlit as st
+import pandas as pd
+from google.cloud import bigquery
+from google.oauth2 import service_account
+
+
+try:
+    creds = service_account.Credentials.from_service_account_info(
+        st.secrets["gcp_service_account"]
+    )
+    client = bigquery.Client(credentials=creds, project=creds.project_id)
+except Exception:
+    # Fallback for local development
+    # GOOGLE_APPLICATION_CREDENTIALS environment variable must be set
+    client = bigquery.Client()
+
+
+@st.cache_data
+def load_raw_data() -> pd.DataFrame:
+    """Queries the BigQuery table and returns the data as a Pandas DataFrame."""
+    # Define your BigQuery SQL query
+    query = """
+        SELECT * FROM `athlete-dashboard-467718.strava_data.strava_activities_raw`
+    """
+    print("Running BigQuery query...")
+    # Run the query and convert the result to a Pandas DataFrame
+    df = client.query(query).to_dataframe()
+    return df
