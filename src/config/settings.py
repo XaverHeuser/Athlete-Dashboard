823c2538f21dcac33d67965c339dfbc25adfe22e
XaverHeuser <xaver.heuser@gmail.com>
2025-10-16 15:51:36 +0200

    ruff

--- src/config/settings.py
+++ src/config/settings.py
@@ -4,6 +4,6 @@ DATA_SOURCE = 'strava'  # Options: 'strava', 'garmin'
 DATA_STORAGE = 'bigquery'  # Options: 'bigquery', 'postgres'
 
 # --- Google Cloud Platform (GCP) Configuration ---
-GCP_PROJECT_ID = "athlete-dashboard-467718"
-BIGQUERY_DATASET = "strava_data"
-BIGQUERY_RAW_TABLE = "strava_data_raw"
+GCP_PROJECT_ID = 'athlete-dashboard-467718'
+BIGQUERY_DATASET = 'strava_data'
+BIGQUERY_RAW_TABLE = 'strava_data_raw'
