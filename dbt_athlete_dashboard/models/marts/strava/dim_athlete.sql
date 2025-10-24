d4140f915f33b3951460aefe0f85eb8abadf72b7
XaverHeuser <xaver.heuser@gmail.com>
2025-10-24 15:19:42 +0200

    create mart for athlete

---
+++ dbt_athlete_dashboard/models/marts/strava/dim_athlete.sql
@@ -0,0 +1,18 @@
+{{ config(materialized='table') }}
+
+SELECT
+    athlete_id,
+    INITCAP(firstname) AS firstname,
+    INITCAP(lastname) AS lastname,
+    COALESCE(bio, '') AS bio,
+    COALESCE(city, 'Unknown') AS city,
+    COALESCE(state, 'Unknown') AS state,
+    COALESCE(country, 'Unknown') AS country,
+    sex,
+    CASE WHEN has_premium OR has_summit THEN TRUE ELSE FALSE END AS is_premium_user,
+    ROUND(weight_kg, 1) AS weight_kg,
+    profile_medium AS profile_medium_img_url,
+    profile AS profile_img_url,
+    created_at,
+    CURRENT_TIMESTAMP() AS mart_loaded_at
+FROM {{ ref('stg_athlete_info') }}
