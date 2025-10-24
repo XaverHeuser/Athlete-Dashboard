219754559b6eea5d5f8e4245e0a1cac697afd65b
XaverHeuser <xaver.heuser@gmail.com>
2025-10-24 12:19:39 +0200

    Enhance base loader

--- src/ingestion/loaders/base.py
+++ src/ingestion/loaders/base.py
@@ -1,12 +1,20 @@
 """This module defines the base interface for all data loaders."""
 
 from abc import ABC, abstractmethod
+from typing import Any
 
 import pandas as pd
 
 
 class BaseLoader(ABC):
     @abstractmethod
-    def load_data(self, data: pd.DataFrame) -> None:
+    def load_data(
+        self,
+        data: pd.DataFrame,
+        dataset: str,
+        table_name: str,
+        *args: Any,
+        **kwargs: Any,
+    ) -> None:
         """Takes data and loads it into the data store."""
         pass
