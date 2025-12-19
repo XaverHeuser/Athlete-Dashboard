3780d8f8b5829e39cc4edf3b4fd13d5b86375c9c
XaverHeuser <xaver.heuser@gmail.com>
2025-12-19 17:21:50 +0100

    Change metrics to km and h

--- dbt/models/marts/strava/fct_activities_yearly.sql
+++ dbt/models/marts/strava/fct_activities_yearly.sql
@@ -4,8 +4,8 @@ SELECT
     sport_type,
     DATE_TRUNC(activity_date, YEAR) AS activity_year,
     SUM(total_activities) AS total_activities,
-    SUM(total_distance_m) AS total_distance_m,
-    SUM(total_moving_time_s) AS total_moving_time_s,
+    SUM(total_distance_km) AS total_distance_km,
+    SUM(total_moving_time_h) AS total_moving_time_h,
     SUM(total_elevation_gain_m) AS total_elevation_gain_m
 FROM {{ ref('fct_activities_daily') }}
 GROUP BY
