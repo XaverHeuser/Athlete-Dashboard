9d59957d6dfa12ad6fefca31998482694a8b858c
XaverHeuser <xaver.heuser@gmail.com>
2026-01-16 19:05:18 +0100

    Prep ml use

---
+++ src/ml/data.py
@@ -0,0 +1,6 @@
+"""
+This py file is used for:
+- BigQuery Zugriff (Query bauen, Sampling, Train/Val/Test-Split)
+- Optional: Spaltenauswahl (Features vs. label vs. ids)
+- Rückgabe als pandas DataFrame oder Arrow/Parquet
+"""
